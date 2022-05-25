a7be99aae3b94cb435b7f88a95b9c0332644fe4a
Leon Schumacher <leonsch@protonmail.com>
2022-05-25 11:43:49 +0200

    Add REPL

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,37 @@ module Main where
 
 import System.Environment
 
+import Control.Monad (when)
+import System.IO     (hFlush, stdout)
+
 import Eval
 import Read
 
 main :: IO ()
 main = do
-  (expr:_) <- getArgs
-  putStrLn $ either
-    (\e -> "ERROR: " ++ show e)
-    show
-    (readExpr expr >>= eval)
+  args <- getArgs
+  if null args
+  then runRepl
+  else evalAndPrint $ head args
+
+evalString :: String -> String
+evalString expr = either
+  (\e -> "ERROR: " ++ show e)
+  show
+  (readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint = flushStr . (++ "\n") . evalString
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+prompt :: String -> IO String
+prompt p = flushStr p >> getLine
+
+runRepl :: IO ()
+runRepl = do
+  expr <- prompt "Lisp> "
+  when (expr /= "quit") $ do
+    evalAndPrint expr
+    runRepl
